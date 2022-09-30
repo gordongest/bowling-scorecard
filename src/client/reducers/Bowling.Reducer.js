cbd20d83f9fcfa6c27f28d9b398669fb412bd37a
Gordon Gest <gordon.gest@gmail.com>
2022-09-30 10:53:48 -0600

    remove console logs and reformat code in reducer

--- src/client/reducers/Bowling.Reducer.js
+++ src/client/reducers/Bowling.Reducer.js
@@ -6,25 +6,24 @@ const bowlingReducer = (state, action) => {
             return [...state, action.player];
         case "addRoll":
             return state.map(player => {
-                console.log("action:", action)
-                    if (player.id === action.playerId) {
-                        player.frames[action.currentFrame][action.currentRoll] = action.rollValue;
+                if (player.id === action.playerId) {
+                    player.frames[action.currentFrame][action.currentRoll] = action.rollValue;
 
-                        if (isStrike(action.rollValue)) {
-                            player.frames[action.currentFrame][action.currentRoll + 1] = "-";
-                        }
-
-                        return player;
+                    if (isStrike(action.rollValue)) {
+                        player.frames[action.currentFrame][action.currentRoll + 1] = "-";
                     }
 
                     return player;
                 }
-            );
+
+                return player;
+            }
+        );
         case "updateTotal":
             return state.map(player =>
                 player.id === action.playerId ?
                     {... player, total: action.total } : player
-            );
+        );
     }
 }
 
